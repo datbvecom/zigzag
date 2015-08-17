0b2572087878eef28ffebb3e1930059db0e98971
dat <dat@dats-MacBook-Pro.local>
2015-08-17 21:32:19 +0700

    Initial Commit

---
+++ zigzag2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  zigzag2
+//
+//  Created by dat on 8/17/15.
+//  Copyright (c) 2015 dat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
